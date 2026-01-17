e61a6792d1941ca0a1cd79f739ff2173425d4fb2
Fachri Ramdhan <92018589+fachriramdhan@users.noreply.github.com>
2026-01-17 18:34:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def detect():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+#if __name__ == "__main__":
+ #   app.run(debug=True, port=5001)
